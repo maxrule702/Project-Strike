59051c149e463dd4864817101f513d501984c169
maxrule702 <maximoo2004@gmail.com>
2022-02-08 21:51:22 +0000

    So far we have a moving square

---
+++ src/com/gaming/GamePanel.java
@@ -0,0 +1,113 @@
+package com.gaming;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements  Runnable {
+    //Screen settings
+    final int oringinalTileSize = 16; // 16*16 tile
+    final int scale = 3; //scaling character
+
+    final int tileSize = oringinalTileSize * scale;
+    final int maxScreenCol = (int) 40;
+    final float maxScreenRow = 22.5f;
+    final int screenwidth = tileSize * maxScreenCol; //1080 pixels
+    final float screenHeight = tileSize * maxScreenRow; //1920 pixels
+
+    //FPS Limter
+    int FPS = 60;
+
+
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread;
+    //set player default location
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
+
+
+
+    public GamePanel(){
+        this.setPreferredSize((new Dimension(screenwidth, (int) screenHeight)));
+        this.setBackground(new Color(255,255,255));
+        this.setDoubleBuffered(true); //performance increase
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+
+
+
+    }
+public void startGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start();
+
+}
+
+    @Override
+    public void run() {
+//This loop runs constantly until it the game is closed
+
+        while(gameThread !=  null){
+
+            double drawInterval = 1000000000/FPS;
+            double delta = 0;
+            long lastTime = System.nanoTime();
+            long currentTime;
+            while(gameThread != null){
+                currentTime = System.nanoTime();
+                delta += (currentTime - lastTime) / drawInterval;
+                lastTime = currentTime;
+
+                if (delta >= 1){
+                    update();
+                    repaint();
+                    delta--;
+                }
+            }
+
+
+update();
+
+repaint();
+
+
+        }
+    }
+//movement
+    public void update(){
+if(keyH.upPressed ==true){
+    playerY -= playerSpeed;
+}
+else if (keyH.downPressed == true){
+    playerY += playerSpeed;
+}
+
+else if (keyH.leftPressed == true){
+    playerX -= playerSpeed;
+}
+
+else if (keyH.rightPressed == true){
+    playerX += playerSpeed;
+}
+
+
+
+
+    }
+
+    public void paintComponent(Graphics g){
+        //repaint()
+        //update graphics
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.blue);
+
+        g2.fillRect(playerX,playerY,tileSize,tileSize);
+        g2.dispose();
+
+
+
+    }
+
+}
--- src/com/gaming/Main.java
+++ src/com/gaming/Main.java
@@ -1,8 +1,23 @@
 package com.gaming;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("bigtest2");
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(true);
+        window.setTitle("2D Game");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        window.pack(); //allows window to be sized to teh fit the perferred size
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamePanel.startGameThread();
+
     }
 }
