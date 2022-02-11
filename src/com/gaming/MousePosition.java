be83d3364ce2d6189f91df5d883d07a2d5aa4009
Matei-Alin Tenche <90476238+Saku2223@users.noreply.github.com>
2022-02-11 20:08:03 +0000

    Mega pain of me trying to get the camera and mouse position thing to work

---
+++ src/com/gaming/MousePosition.java
@@ -0,0 +1,13 @@
+package com.gaming;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.event.MouseMotionListener;
+
+public class MousePosition {
+
+    public void mouseMoved(MouseEvent e){
+        
+    }
+
+}
