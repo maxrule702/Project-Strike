1f10fb499cacd48cf57520540eaae3cfd487a221
mt210673 <mt210673@reigate.ac.uk>
2022-02-22 16:07:56 +0000

    adding crosshair to the game

---
+++ src/com/gaming/Crosshair.java
@@ -0,0 +1,18 @@
+package com.gaming;
+
+import java.io.IOException;
+import main.GamePanel;
+
+public class Crosshair {
+
+    GamePanel gp;
+    Crosshair[] crosshair;
+
+    public Crosshair(GamePanel gp) {
+
+        this.gp = gp;
+
+        crosshair = new Crosshair[1];
+
+    }
+}
