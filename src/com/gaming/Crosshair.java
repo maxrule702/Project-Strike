7b8cfac4084298ad0a6864b209a499bca57b30c6
mt210673 <mt210673@reigate.ac.uk>
2022-02-22 16:15:45 +0000

    crosshair being added

--- src/com/gaming/Crosshair.java
+++ src/com/gaming/Crosshair.java
@@ -1,6 +1,9 @@
 package com.gaming;
 
+import java.awt.image.BufferedImage;
 import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import main.GamePanel;
 
 public class Crosshair {
@@ -13,6 +16,20 @@ public class Crosshair {
         this.gp = gp;
 
         crosshair = new Crosshair[1];
+
+        getCrosshairImage();
+    }
+    public BufferedImage up1,down1,left1,right1,image1,image2,image3,image4,image5,image6;
+
+    public void getCrosshairImage() {
+        try {
+
+            crosshair[0] = new Crosshair();
+            crosshair[0].image1 = ImageIO.read(getClass() .getResourceAsStream("/player/crosshairPrototype.png"));
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
