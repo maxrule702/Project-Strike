106ff6954a0f04159da6e0c5bd21bd0802b62e53
Matei-Alin Tenche <90476238+Saku2223@users.noreply.github.com>
2022-03-01 17:38:53 +0000

    Added music to the game

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,19 +1,21 @@
 package main;
+
 import entity.Player;
 import objects.SuperObject;
+import sound.Sound;
 import tiles.tileManger;
 
 import javax.swing.*;
 import java.awt.*;
 
 
-public class GamePanel extends JPanel implements  Runnable {
+public class GamePanel extends JPanel implements Runnable {
     //Screen settings
     final int oringinalTileSize = 32; // 16*16 tile
     final int scale = 2; //scaling character
 
     public final int tileSize = oringinalTileSize * scale;
-    public final int maxScreenCol =  24;
+    public final int maxScreenCol = 24;
     public final float maxScreenRow = 14F;
     public final int screenwidth = tileSize * maxScreenCol; //1080 pixels
     public final float screenHeight = tileSize * maxScreenRow; //1920 pixels
@@ -25,19 +27,17 @@ public class GamePanel extends JPanel implements  Runnable {
     public final int worldHeight = tileSize * maxWorldRow;
 
 
-
-
     //FPS Limter
     int FPS = 60;
 
     tileManger tileM = new tileManger(this);
     KeyHandler keyH = new KeyHandler();
+    Sound sound = new Sound();
     Thread gameThread;
-    public  collisionChecker cChecker = new collisionChecker(this);
+    public collisionChecker cChecker = new collisionChecker(this);
     public assetSetter aSetter = new assetSetter(this);
-    public Player player = new Player(this,keyH);
+    public Player player = new Player(this, keyH);
     public SuperObject obj[] = new SuperObject[10];
-
 
 
     //set player default location
@@ -46,37 +46,36 @@ public class GamePanel extends JPanel implements  Runnable {
     int playerSpeed = 4;
 
 
-
-
-    public GamePanel(){
+    public GamePanel() {
         this.setPreferredSize((new Dimension((int) screenwidth, (int) screenHeight)));
-        this.setBackground(new Color(255,255,255));
+        this.setBackground(new Color(255, 255, 255));
         this.setDoubleBuffered(true); //performance increase
         this.addKeyListener(keyH);
         this.setFocusable(true);
 
 
-
     }
 
-    public void setupGame(){
+    public void setupGame() {
         aSetter.setObject();
 
+        playMusic(0);
+
     }
 
-public void startGameThread(){
+    public void startGameThread() {
         gameThread = new Thread(this);
         gameThread.start();
 
-}
+    }
 
     @Override
     public void run() {
 //This loop runs constantly until it the game is closed
 
-        while(gameThread !=  null){
+        while (gameThread != null) {
 
-            double drawInterval = 1000000000/FPS;
+            double drawInterval = 1000000000 / FPS;
             double delta = 0;
             long lastTime = System.nanoTime();
             long currentTime;
@@ -84,20 +83,19 @@ public void startGameThread(){
             int drawCount = 0;
 
 
-
-            while(gameThread != null){
+            while (gameThread != null) {
                 currentTime = System.nanoTime();
                 timer += (currentTime - lastTime);
                 delta += (currentTime - lastTime) / drawInterval;
                 lastTime = currentTime;
 
-                if (delta >= 1){
+                if (delta >= 1) {
                     update();
                     repaint();
                     delta--;
                     drawCount++;
                 }
-                if (timer >= 1000000000){
+                if (timer >= 1000000000) {
                     System.out.println("FPS " + drawCount);
                     drawCount = 0;
                     timer = 0;
@@ -105,29 +103,29 @@ public void startGameThread(){
             }
 
 
-update();
+            update();
 
-repaint();
+            repaint();
 
 
         }
     }
-//movement
-    public void update(){
+
+    //movement
+    public void update() {
         player.update();
     }
 
 
-
-    public void paintComponent(Graphics g){
+    public void paintComponent(Graphics g) {
         //repaint()
         //update graphics
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
         tileM.draw(g2);
-        for (int i = 0; i < obj.length ; i++) {
-            if (obj[i]!= null){
-                obj[i].draw(g2,this);
+        for (int i = 0; i < obj.length; i++) {
+            if (obj[i] != null) {
+                obj[i].draw(g2, this);
             }
 
         }
@@ -135,7 +133,22 @@ repaint();
         g2.dispose();
 
 
-
     }
 
+    public void playMusic(int i) {
+
+        sound.setFile(i);
+        sound.play();
+        sound.loop();
+    }
+
+    public void stopMusic() {
+
+        sound.stop();
+    }
+
+    public void playSE(int i) {
+
+        sound.setFile(i);
+    }
 }
---
+++ src/sound/Sound.java
@@ -0,0 +1,46 @@
+package sound;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.net.URL;
+
+public class Sound {
+
+    Clip clip;
+    URL soundURL[] = new URL[30];
+
+    public Sound() {
+        //Sound files to go in the sound folder, and then the sound name will go after the "/sound/" bit
+        soundURL[0] = getClass().getResource("/sound/GameMusic.wav");
+
+    }
+
+    public void setFile(int i) {
+
+        try {
+            AudioInputStream ais = AudioSystem.getAudioInputStream(soundURL[i]);
+            clip = AudioSystem.getClip();
+            clip.open(ais);
+
+        } catch (Exception e) {
+
+        }
+    }
+
+    public void play() {
+
+        clip.start();
+    }
+
+    public void loop() {
+
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+
+    public void stop() {
+
+        clip.stop();
+    }
+
+}
