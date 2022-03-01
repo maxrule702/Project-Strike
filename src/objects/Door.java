805f0353e72b5991f163e2f6b3ef072245d3556d
maxrule702 <maximoo2004@gmail.com>
2022-03-01 16:13:15 +0000

    bug fixes

---
+++ src/objects/Door.java
@@ -0,0 +1,23 @@
+package objects;
+
+import javax.imageio.ImageIO;
+
+public class Door extends  SuperObject{
+    public Door() {
+        name = "door";
+        try {
+            image = ImageIO.read(getClass().getResourceAsStream("/objects/door.png"));
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
+
+
+
+
+
