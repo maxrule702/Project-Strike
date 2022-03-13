6cf22e88e1f6b1a8072a02000f393b1f972b47f0
Matei-Alin Tenche <90476238+Saku2223@users.noreply.github.com>
2022-03-13 16:03:52 +0000

    Tried to fix Pause screen, ended up fixing typos

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -166,8 +166,8 @@ public class GamePanel extends JPanel implements Runnable {
         if (keyH.checkDrawTime == true){
 
 
-        long drew = System.nanoTime();
-        long passed = drew - drawStart;
+        long drawn = System.nanoTime();
+        long passed = drawn - drawStart;
         g2.setColor(Color.white);
         g2.drawString ("draw time:" + passed,10,400);
         System.out.println("draw time;" + passed);
